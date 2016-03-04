0293a474bdb151eda2a137c839e79eb11f610669
Chris Kalafarski <chris@farski.com>
2016-03-04 10:22:42 -0500

    Remove unnecessary parens from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,19 +14,19 @@ let shell         = require('gulp-shell');
 let sourcemaps    = require('gulp-sourcemaps');
 
 // Public tasks (serial)
-gulp.task('deploy', (cb) => run('build:dist', 'preinstall:dist', 'install:dist', 'postinstall:dist', cb));
-gulp.task('start', (cb) => run('build:dev', 'server:dev', cb));
-gulp.task('start:dist', (cb) => run('build:dist', 'server:dist', cb));
-gulp.task('test', (cb) => run('server:test', cb));
+gulp.task('deploy', cb => run('build:dist', 'preinstall:dist', 'install:dist', 'postinstall:dist', cb));
+gulp.task('start', cb => run('build:dev', 'server:dev', cb));
+gulp.task('start:dist', cb => run('build:dist', 'server:dist', cb));
+gulp.task('test', cb => run('server:test', cb));
 
 // Build tasks (parallel)
-gulp.task('build:dev', (cb) => run(['jade:index:dev', 'jspm:bundle:dev'], cb));
-gulp.task('build:dist', (cb) => run(['copy:assets:dist', 'copy:vendor:dist', 'jade:index:dist', 'jspm:bundle:dist'], cb));
+gulp.task('build:dev', cb => run(['jade:index:dev', 'jspm:bundle:dev'], cb));
+gulp.task('build:dist', cb => run(['copy:assets:dist', 'copy:vendor:dist', 'jade:index:dist', 'jspm:bundle:dist'], cb));
 
 // Deploy tasks (serial)
-gulp.task('install:dist', (cb) => run('push:s3:dist', cb));
-gulp.task('preinstall:dist', (cb) => run('version:bump', 'version:mark:dist', cb));
-gulp.task('postinstall:dist', (cb) => run('push:s3:version', cb));
+gulp.task('install:dist', cb => run('push:s3:dist', cb));
+gulp.task('preinstall:dist', cb => run('version:bump', 'version:mark:dist', cb));
+gulp.task('postinstall:dist', cb => run('push:s3:version', cb));
 
 // Server tasks
 gulp.task('server:dev', shell.task(['lite-server --config=config/dev.bs.config.json']));
@@ -38,7 +38,7 @@ gulp.task('jspm:bundle:dev', shell.task([
   'jspm bundle src/main - [src/app/**/*] ./.dev/vendor.js --inject'
 ]));
 
-gulp.task('jspm:bundle:dist', (callback) => {
+gulp.task('jspm:bundle:dist', callback => {
   return gulp
     .src('./src/main.ts')
     .pipe(sourcemaps.init())
@@ -49,14 +49,14 @@ gulp.task('jspm:bundle:dist', (callback) => {
 });
 
 // Jade compile tasks
-gulp.task('jade:index:dev', (callback) => {
+gulp.task('jade:index:dev', callback => {
   return gulp
     .src('./src/index.jade')
     .pipe(jade({ locals: { release: false } }))
     .pipe(gulp.dest('./src/'));
 });
 
-gulp.task('jade:index:dist', (callback) => {
+gulp.task('jade:index:dist', callback => {
   return gulp
     .src('./src/index.jade')
     .pipe(jade({ locals: { release: true } }))
@@ -64,39 +64,39 @@ gulp.task('jade:index:dist', (callback) => {
 });
 
 // Copy tasks
-gulp.task('copy:assets:dist', (callback) => {
+gulp.task('copy:assets:dist', callback => {
   return gulp
     .src(['./src/**/*.html', '!./src/index.html', './src/**/*.css', './src/images/**/*'], { base: 'src' })
     .pipe(gulp.dest('./.dist/'));
 });
 
-gulp.task('copy:vendor:dist', (callback) => {
+gulp.task('copy:vendor:dist', callback => {
   return gulp
     .src('./node_modules/angular2/bundles/angular2-polyfills.js')
     .pipe(gulp.dest('./.dist/scripts/'));
 });
 
 // Utility tasks
-gulp.task('push:s3:dist', (callback) => {
+gulp.task('push:s3:dist', callback => {
   return gulp
     .src(['./.dist/**', '!./.dist/version.deploy'])
     .pipe(s3({ Bucket: 'embed.prx.org' }));
 });
 
-gulp.task('push:s3:version', (callback) => {
+gulp.task('push:s3:version', callback => {
   return gulp
     .src('./.dist/version.deploy')
     .pipe(s3({ Bucket: 'embed.prx.org' }));
 });
 
-gulp.task('version:bump', (callback) => {
+gulp.task('version:bump', callback => {
   return gulp
     .src('./package.json')
     .pipe(bump())
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('version:mark:dist', function (callback) {
+gulp.task('version:mark:dist', callback => {
   let pkg = require('./package.json');
   return file('version.deploy', pkg.version, { src: true })
     .pipe(gulp.dest('./.dist/'));
