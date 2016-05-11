6090e3234e5264a8efa9c7674c0108d4ca82d7a4
Chris Kalafarski <chris@farski.com>
2016-05-11 14:34:58 -0400

    WIP

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,5 @@
-// import {AppComponent} from './app.component';
-//
-// describe('application', () => {
-//   it('has main app component', () => expect(AppComponent).not.toBeNull());
-// });
-//
+import {describe, expect, it} from 'angular2/testing';
 
-import {
-  describe,
-  expect,
-  it
-} from 'angular2/testing';
-
-describe('the test', () => {
-  it('should pass', () => {
-    expect(1).toEqual(1);
-  });
-
-  it('should fail', () => {
-    expect(1).toEqual(0);
-  });
+describe('test canary', () => {
+  it('should always pass', () => expect(1).toEqual(1));
 });
