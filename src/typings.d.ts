68f75991f3fd2af20ea8451640a5c0c5ccea3af6
cavis <ryancavis@gmail.com>
2016-12-12 21:59:31 -0700

    Downgrade cli slightly to match Publish

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,20 +2,3 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
-
-// extend jasmine matchers
-declare module jasmine {
-  interface Matchers {
-    toHaveText(expected: string): boolean;
-    toContainText(expected: string): boolean;
-    toQuery(cssQuery: string): boolean;
-    toQueryAttr(cssQuery: string, attrName: string, expected: string): boolean;
-    toQueryText(cssQuery: string, expected: string): boolean;
-  }
-}
-
-// TODO: shouldn't need this, but the getter in testing/index.ts warns without
-interface NodeModule {
-  exports: any;
-}
-declare var module: NodeModule;
