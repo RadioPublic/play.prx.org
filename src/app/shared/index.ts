dbb986c208cc1670686b8297003458f97cb4bffb
cavis <ryancavis@gmail.com>
2016-12-13 08:45:08 -0700

    Avoid circular includes

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,8 +1,1 @@
 export * from './shared.module';
-
-export * from './adzerk';
-export * from './audio';
-export * from './dovetail';
-export * from './duration';
-export * from './logger';
-export * from './player';
