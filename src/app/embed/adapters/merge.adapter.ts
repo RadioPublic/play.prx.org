218b7e18c41a78a8b1e885875361cfa978fb870b
Alex Jarvis <alxjrvs@gmail.com>
2016-12-17 10:08:09 -0500

    another stab at the same concept.

--- src/app/embed/adapters/merge.adapter.ts
+++ src/app/embed/adapters/merge.adapter.ts
@@ -2,7 +2,7 @@ import { Http } from '@angular/http';
 import { EMBED_AUDIO_URL_PARAM, EMBED_TITLE_PARAM, EMBED_SUBTITLE_PARAM,
   EMBED_SUBSCRIBE_URL_PARAM, EMBED_SUBSCRIBE_TARGET, EMBED_IMAGE_URL_PARAM, EMBED_FEED_ID_PARAM, EMBED_EPISODE_GUID_PARAM } from './../embed.constants';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
 import { QSDAdapter } from './qsd.adapter'
 import { FeedAdapter } from './feed.adapter'
@@ -22,15 +22,16 @@ export class MergeAdapter {
   }
 
   get getProperties(): Observable<Object> {
-    return new Observable(obs => {
-      this.FeedAdapter.getProperties.subscribe( properties => {
-        obs.next(properties)
+    let source = new ReplaySubject();
+    let adapters = [this.QSDAdapter, this.FeedAdapter]
+    adapters.forEach( (adapter) => {
+      adapter.getProperties.subscribe( properties => {
+        console.log("props...FA");
+        source.next(properties)
       })
-      this.QSDAdapter.getProperties.subscribe( properties => {
-        obs.next(properties)
-      })
-      return obs
+      
     })
+    return source;
 	}
 }
 
