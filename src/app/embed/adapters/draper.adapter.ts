29bc9cd6b8badb1ac291e63ebe06be5e33eee4a6
chrisrhoden <carhoden@gmail.com>
2017-01-31 14:29:57 -0500

    Make draper feeds & eps direct to RP landing pages. Fix display issues.
    I also changed things to use _top. Our embeds are sandboxed and _blank
    is literally not an option - it will turn the link into a noop, which
    is already confusing and irritating users. It's for the best, since these
    links were previously directing to RSS feeds, so there's that.

--- src/app/embed/adapters/draper.adapter.ts
+++ src/app/embed/adapters/draper.adapter.ts
@@ -22,10 +22,24 @@ export class DraperAdapter extends FeedAdapter {
     }
   }
 
+  processFeed(feedUrl: string, episodeGuid?: string): Observable<AdapterProperties> {
+    return super.processFeed(feedUrl, episodeGuid).map(props => {
+      props.subscribeTarget = "_top";
+      if (episodeGuid) {
+        if (!this.isEncoded(episodeGuid)) {
+          episodeGuid = this.encodeGuid(episodeGuid);
+        }
+        props.subscribeUrl = `${props.subscribeUrl}/ep/${episodeGuid}`;
+      }
+      return props;
+    });
+  }
+
   processDoc(doc: XMLDocument, props: AdapterProperties = {}): AdapterProperties {
     props = super.processDoc(doc, props);
     props.feedArtworkUrl = this.getTagAttributeNS(doc, 'rp', 'image', 'href')
                         || props.feedArtworkUrl;
+    props.subscribeUrl = `https://play.radiopublic.com/${this.getTagTextNS(doc, 'rp', 'program-id')}`;
     return props;
   }
 
