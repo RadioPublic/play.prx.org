a471e4b9a15eda0f5c63eebd52025af3a8c68e43
gcampo88 <gigi.campo@gmail.com>
2017-06-01 16:22:14 -0400

    Test

--- src/app/shared/playlist/playlist.component.ts
+++ src/app/shared/playlist/playlist.component.ts
@@ -22,7 +22,7 @@ export class PlaylistComponent {
     const secPerUnit = { d: 86400, h: 3600, m: 60 };
     let timeEst = '';
     Object.keys(secPerUnit).forEach(unit => {
-      if (seconds / secPerUnit[unit] > 1) {
+      if (seconds / secPerUnit[unit] >= 1) {
         timeEst += `${Math.floor(seconds / secPerUnit[unit])}${unit} `;
         seconds %= secPerUnit[unit];
       }
