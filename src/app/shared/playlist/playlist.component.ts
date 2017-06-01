0092f68456cecd31f7ceb3da34d1bd110eacd1e1
gcampo88 <gigi.campo@gmail.com>
2017-06-01 15:42:46 -0400

    Round the durations for playlist and playlist eps

--- src/app/shared/playlist/playlist.component.ts
+++ src/app/shared/playlist/playlist.component.ts
@@ -18,4 +18,15 @@ export class PlaylistComponent {
     return this.episodes.reduce((accum, ep) => accum + +ep.duration, 0);
   }
 
+  estDuration(seconds: number): string {
+    const secPerUnit = { d: 86400, h: 3600, m: 60 };
+    let timeEst = '';
+    Object.keys(secPerUnit).forEach(unit => {
+      if (seconds / secPerUnit[unit] > 1) {
+        timeEst += `${Math.floor(seconds / secPerUnit[unit])}${unit} `;
+        seconds %= secPerUnit[unit];
+      }
+    });
+    return timeEst.trim();
+  }
 }
