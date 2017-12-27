35c07726dfaa082e27241437f0311c81f7eedd5f
cavis <ryancavis@gmail.com>
2017-12-27 16:25:38 -0700

    First shot at outsourcing segments

--- src/app/shared/dovetail/index.ts
+++ src/app/shared/dovetail/index.ts
@@ -1,5 +1,5 @@
-export * from './dovetail-arrangement';
 export * from './dovetail-audio-event';
 export * from './dovetail-audio';
 export * from './dovetail-errors';
 export * from './dovetail-fetcher';
+export * from './dovetail-segment';
